e092bcdd33885c50e48b2565296209c7e561bcd4
deatrisya <mirelldee47@gmail.com>
2022-03-18 11:17:28 +0700

    Add item controller with function

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+
+class ItemController extends Controller
+{
+    public function item(){
+        $dataitem = Item::all();
+        
+        return view('item')
+            -> with('item',$dataitem);
+    }
+}
