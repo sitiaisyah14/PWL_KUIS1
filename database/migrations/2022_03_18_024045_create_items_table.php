760e9fb39737a869677a289f463b8b295f9a4070
deatrisya <mirelldee47@gmail.com>
2022-03-18 11:18:28 +0700

    Add item migration with table

---
+++ database/migrations/2022_03_18_024045_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_supplier');
+            $table->string('gambar_sepatu',255);
+            $table->string('merk_sepatu',255);
+            $table->string('jenis_sepatu',255);
+            $table->integer('no_sepatu');
+            $table->integer('stok');
+            $table->double('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
