cba45f8769679f0bc520c45e5cec1e20f6362968
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-18 09:00:15 +0700

    create a navbar view

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,26 @@
+<header class="">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="{{Route('home')}}"><h2>aisyde   <em>Shoes</em></h2></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                    <a class="nav-link" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#products">Our Products</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#about">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
