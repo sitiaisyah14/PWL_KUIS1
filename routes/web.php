381c579ed628fee9ad8029eabb797abb588cc481
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-18 09:25:14 +0700

    Add route home and products

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductsController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('products', [ProductsController::class, 'products'])->name('products');
