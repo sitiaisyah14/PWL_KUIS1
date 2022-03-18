30ea47896355bbd477b2d673cdba7770ac115a6f
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-18 12:42:22 +0700

    Modify Controller - to call the model to display the table employee

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\supplier;
 
 class SupplierController extends Controller
 {
     public function supplier(){
-        return view('supplier');
+        $data = supplier::all();
+        return view('supplier', ['supplier'=>$data]);
     }
 }
