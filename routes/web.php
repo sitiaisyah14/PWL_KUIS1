865e2b0c408038d10e665c47ec01fddd568f859c
deatrisya <mirelldee47@gmail.com>
2022-03-18 09:32:01 +0700

    Add new route about and contact

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('products', [ProductsController::class, 'products'])->name('products');
+Route::get('about', [AboutController::class, 'about'])->name('about');
+Route::get('contact', [ContactController::class, 'contact'])->name('contact');
