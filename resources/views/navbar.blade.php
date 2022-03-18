170964a969e43496903cf9fa28ff3ee0a9c17046
deatrisya <mirelldee47@gmail.com>
2022-03-18 09:16:51 +0700

    Add id at navlink

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,23 +1,23 @@
 <header class="">
     <nav class="navbar navbar-expand-lg">
         <div class="container">
-            <a class="navbar-brand" href="{{Route('home')}}"><h2>aisyde   <em>Shoes</em></h2></a>
+            <a class="navbar-brand" href="{{route('home')}}"><h2>aisyde   <em>Shoes</em></h2></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                    <a class="nav-link" href="#home">Home</a>
+                    <a class="nav-link" href="#home" id="home">Home</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#products">Our Products</a>
+                    <a class="nav-link" href="#products" id="product">Our Products</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#about">About Us</a>
+                    <a class="nav-link" href="#about" id="about">About Us</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#contact">Contact Us</a>
+                    <a class="nav-link" href="#contact" id="contact">Contact Us</a>
                     </li>
                 </ul>
             </div>
