c442febb6387ad93b8541d8de68354d5addb26da
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-18 12:24:22 +0700

    DatabaseSeeder - calling process on SupplierSeeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,10 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
+            SupplierSeeder::class,
+        ]);
+
+        $this->call([
             EmployeeSeeder::class,
         ]);
 
