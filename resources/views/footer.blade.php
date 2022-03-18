3f38f3a7e35dd9e46b8b1701f89076e782240d98
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-18 09:00:40 +0700

    create a footer view

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="inner-content">
+            <p>Copyright &copy; 2022 Aide Shoes
+
+          - Design: <a rel="nofollow noopener" target="_blank">Aisyah & Dea</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+</footer>
