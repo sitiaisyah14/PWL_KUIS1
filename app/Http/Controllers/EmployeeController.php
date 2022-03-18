4d6e92c62190db6de6d271f350a9610c755473c5
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-18 10:15:29 +0700

    modify - to call the model to display the table employee

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\employe;
 
 class EmployeeController extends Controller
 {
     public function employee(){
-        return view('employee');
+        $data = employe::all();
+        return view('employe', ['employe'=>$data]);
     }
 }
