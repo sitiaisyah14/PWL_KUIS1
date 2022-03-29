23905e857c01a70761f4d9a95056796bef13a215
deatrisya <mirelldee47@gmail.com>
2022-03-29 14:25:39 +0700

    Modification Form login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,13 +12,13 @@
 <body>
     <div class="login-page">
         <div class="form">
-            <form class="register-form">
+            {{-- <form class="register-form">
                 <input type="text" placeholder="name" />
                 <input type="password" placeholder="password" />
                 <input type="text" placeholder="email address" />
                 <button>create</button>
                 <p class="message">Already registered? <a href="#">Sign In</a></p>
-            </form>
+            </form> --}}
             <form class="login-form">
                 <input type="text" placeholder="username" />
                 <input type="password" placeholder="password" />
