572ab390793d62ffa666889adcf89c0cf8e1ff22
deatrisya <mirelldee47@gmail.com>
2022-03-29 13:32:19 +0700

    Add test login function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,4 +9,8 @@ class HomeController extends Controller
     public function index(){
         return view('index');
     }
+    public function login()
+    {
+        return view('login');
+    }
 }
