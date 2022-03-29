3783ab8ecd450af06d3d579d6f6d4011cc8c2726
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-29 14:27:00 +0700

    Add view register

---
+++ resources/views/register.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aide|Register</title>
+    <link rel="stylesheet" href="{{asset('css/register-style.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="register-page">
+        <div class="form">
+            <form>
+                <div class="form-group">
+                    <label for="formGroupExampleInput">Nama</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput"
+                        placeholder="Example input placeholder">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Username</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput2"
+                        placeholder="Another input placeholder">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Password</label>
+                    <input type="password" class="form-control" id="formGroupExampleInput2"
+                        placeholder="Another input placeholder">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Gender</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput2"
+                        placeholder="Another input placeholder">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Place of Birth</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput2"
+                        placeholder="Another input placeholder">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Date of Birth</label>
+                    <input type="date" class="form-control" id="formGroupExampleInput2"
+                        placeholder="Another input placeholder">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Date of Birth</label>
+                    <input type="date" class="form-control" id="formGroupExampleInput2"
+                        placeholder="Another input placeholder">
+                </div>
+                <div>
+                    <button>create</button>
+                    <p class="message">Already registered? <a href="#">Sign In</a></p>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
