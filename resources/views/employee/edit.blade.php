ee0592c33b10036ea97cdd8977496b3ffb3382d9
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-30 18:20:32 +0700

    add and modify view for edit data employee

---
+++ resources/views/employee/edit.blade.php
@@ -0,0 +1,76 @@
+@extends('layout 2.layout')
+
+@section('content')
+<div class="container mt-5">
+    <div class="row justify-content-center align-items-center">
+        <div class="card" style="width: 24rem;">
+            <div class="card-header">
+                Edit Supplier
+            </div>
+            <div class="card-body">
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+                <form method="post" action="{{ route('employee.update', $Employee->id)}}" id="myForm">
+                    @csrf
+                    @method('PUT')
+                    <div class="form-group">
+                        <div class="form-group">
+                            <label for="name">Name</label> <br>
+                            <input type="text" name="name" class="form-control" id="name" value="{{$Employee->name}}" aria-describedby="name">
+                        </div>
+                        <div class="form-group">
+                            <label for="username">Username</label> <br>
+                            <input type="text" name="username" class="form-control" id="username" value="{{$Employee->username}}" aria-describedby="username">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label> <br>
+                            <input type="text" name="password" class="form-control" id="password" value="{{$Employee->password}}" aria-describedby="password">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label> <br>
+                            <input type="text" name="email" class="form-control" id="email" value="{{$Employee->email}}" aria-describedby="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="gender">Gender</label> <br>
+                            <input type="text" name="gender" class="form-control" id="gender" value="{{$Employee->gender}}" aria-describedby="gender">
+                        </div>
+                        <div class="form-group">
+                            <label for="place_of_birth">Place Of Birth</label> <br>
+                            <input type="text" name="place_of_birth" class="form-control" id="place_of_birth" value="{{$Employee->place_of_birth}}" aria-describedby="place_of_birth">
+                        </div>
+                        <div class="form-group">
+                            <label for="date_of_birth">Date Of Birth</label> <br>
+                            <input type="text" name="date_of_birth" class="form-control" id="date_of_birth" value="{{$Employee->date_of_birth}}" aria-describedby="date_of_birth">
+                        </div>
+                        <div class="form-group">
+                            <label for="phone_number">Phone Number</label> <br>
+                            <input type="text" name="phone_number" class="form-control" id="phone_number" value="{{$Employee->phone_number}}" aria-describedby="phone_number">
+                        </div>
+                        <div class="form-group">
+                            <label for="status">Status</label> <br>
+                            <input type="text" name="status" class="form-control" id="status" value="{{$Employee->status}}" aria-describedby="status">
+                        </div>
+                        <div class="form-group">
+                            <label for="position">Position</label> <br>
+                            <input type="text" name="position" class="form-control" id="position" value="{{$Employee->position}}" aria-describedby="position">
+                        </div>
+                        <div class="form-group">
+                            <label for="wages">Wages</label> <br>
+                            <input type="text" name="wages" class="form-control" id="wages" value="{{$Employee->wages}}" aria-describedby="wages">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
