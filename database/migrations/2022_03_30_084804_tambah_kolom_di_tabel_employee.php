dc366052c74eb762484e2ea07724ccfde691e6a6
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-30 16:06:55 +0700

    add column in table employees

---
+++ database/migrations/2022_03_30_084804_tambah_kolom_di_tabel_employee.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TambahKolomDiTabelEmployee extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->string('username', 20)->after('name')->nullable()->unique();
+            $table->string('password', 20)->after('username')->nullable();
+            $table->string('email', 50)->after('password')->nullable()->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->$table->dropColumn('username');
+            $table->$table->dropColumn('password');
+            $table->$table->dropColumn('email');
+        });
+    }
+}
