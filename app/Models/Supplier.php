3d59f9bd99638079214ac1ab82645c662ec8520b
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-30 11:13:18 +0700

    Modify Model Supplier and add $fillable

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -8,7 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Supplier extends Model
 {
     use HasFactory;
-    
+
     protected $table = 'suppliers';
     protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'id',
+        'name',
+        'company_name',
+        'address',
+        'districts',
+        'province',
+        'postal_code',
+        'phone_number',
+        'bank',
+        'no_rek',
+    ];
 }
