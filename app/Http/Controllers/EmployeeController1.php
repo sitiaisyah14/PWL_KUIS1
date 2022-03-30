ea042ae972488baf2568e9dbed2d91c83373aecc
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-30 18:18:05 +0700

    Rename controller old

---
+++ app/Http/Controllers/EmployeeController1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\employee;
+
+class EmployeeController extends Controller
+{
+    public function employee(){
+        $data = employee::paginate(5);
+        return view('employee.employee', ['employee'=>$data]);
+    }
+}
