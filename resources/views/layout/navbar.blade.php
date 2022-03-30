8add4bb0c652632a11f666c32ea8c114a10095af
deatrisya <mirelldee47@gmail.com>
2022-03-30 10:15:48 +0700

    Mengubah route item

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -20,7 +20,7 @@
                     <a class="nav-link" href="{{route('contact')}}" id="contact">Contact</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{route('item')}}" id="item">Item</a>
+                        <a class="nav-link" href="{{route('item.index')}}" id="item">Item</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{route('customer')}}" id="customer">Customer</a>
