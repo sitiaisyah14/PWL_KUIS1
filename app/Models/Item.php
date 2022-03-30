6667f016deb0726afaff3262d7a5daf02d95dbc7
deatrisya <mirelldee47@gmail.com>
2022-03-30 10:15:06 +0700

    Menambahkan fillable

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,12 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\Item as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Item extends Model
 {
-    use HasFactory;
     protected $table ='items';
     protected $primaryKey ='id';
+
+    protected $fillable =[
+        'id_supplier',
+        'gambar_sepatu',
+        'merk_sepatu',
+        'jenis_sepatu',
+        'no_sepatu',
+        'stok',
+        'harga',
+    ];
+    use HasFactory;
 }
