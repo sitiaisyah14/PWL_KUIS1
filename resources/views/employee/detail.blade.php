a69c9efb85243099416d80036564917fd30b3795
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-30 19:22:28 +0700

    Modify form label in Detail employee

--- resources/views/employee/detail.blade.php
+++ resources/views/employee/detail.blade.php
@@ -3,28 +3,60 @@
 @section('content')
 <div class="container mt-5">
     <div class="row justify-content-center align-items-center">
-        <div class="card" style="width: 30rem;">
+        <div class="card" style="width: 50rem;">
             <div class="card-header">
-                Detail Supplier
+                <b>Detail Employee</b>
             </div>
             <div class="card-body">
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><b>Id: </b>{{$Employee->id}}</li>
                     <div class="form-group">
                         <label for="id">ID</label> <br>
                         <input type="text" name="id" class="form-control" id="id" value="{{$Employee->id}}" readonly aria-describedby="id">
                     </div>
-                    <li class="list-group-item"><b>Name: </b>{{$Employee->name}}</li>
-                    <li class="list-group-item"><b>Username: </b>{{$Employee->username}}</li>
-                    <li class="list-group-item"><b>Password: </b>{{$Employee->password}}</li>
-                    <li class="list-group-item"><b>Email: </b>{{$Employee->email}}</li>
-                    <li class="list-group-item"><b>Gender: </b>{{$Employee->gender}}</li>
-                    <li class="list-group-item"><b>Place Of Birth : </b>{{$Employee->place_of_birth}}</li>
-                    <li class="list-group-item"><b>Date Of Birth: </b>{{$Employee->date_of_birth}}</li>
-                    <li class="list-group-item"><b>Phone: </b>{{$Employee->phone_number}}</li>
-                    <li class="list-group-item"><b>Status: </b>{{$Employee->status}}</li>
-                    <li class="list-group-item"><b>Position: </b>{{$Employee->position}}</li>
-                    <li class="list-group-item"><b>Wages: </b>{{$Employee->wages}}</li>
+                    <div class="form-group">
+                        <label for="name">Name</label> <br>
+                        <input type="text" name="name" class="form-control" id="name" value="{{$Employee->name}}" readonly aria-describedby="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="username">Username</label> <br>
+                        <input type="text" name="username" class="form-control" id="username" value="{{$Employee->username}}" readonly aria-describedby="username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label> <br>
+                        <input type="text" name="password" class="form-control" id="password" value="{{$Employee->password}}" readonly aria-describedby="password">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label> <br>
+                        <input type="text" name="email" class="form-control" id="email" value="{{$Employee->email}}" readonly aria-describedby="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="gender">Gender</label> <br>
+                        <input type="text" name="gender" class="form-control" id="gender" value="{{$Employee->gender}}" readonly aria-describedby="gender">
+                    </div>
+                    <div class="form-group">
+                        <label for="place_of_birth">Place Of Birth</label> <br>
+                        <input type="text" name="place_of_birth" class="form-control" id="place_of_birth" value="{{$Employee->place_of_birth}}" readonly aria-describedby="place_of_birth">
+                    </div>
+                    <div class="form-group">
+                        <label for="date_of_birth">Date Of Birth</label> <br>
+                        <input type="text" name="date_of_birth" class="form-control" id="date_of_birth" value="{{$Employee->date_of_birth}}" readonly aria-describedby="date_of_birth">
+                    </div>
+                    <div class="form-group">
+                        <label for="phone_number">Phone Number</label> <br>
+                        <input type="text" name="phone_number" class="form-control" id="phone_number" value="{{$Employee->phone_number}}" readonly aria-describedby="phone_number">
+                    </div>
+                    <div class="form-group">
+                        <label for="status">Status</label> <br>
+                        <input type="text" name="status" class="form-control" id="status" value="{{$Employee->status}}" readonly aria-describedby="status">
+                    </div>
+                    <div class="form-group">
+                        <label for="position">Position</label> <br>
+                        <input type="text" name="position" class="form-control" id="position" value="{{$Employee->position}}" readonly aria-describedby="position">
+                    </div>
+                    <div class="form-group">
+                        <label for="wages">Wages</label> <br>
+                        <input type="text" name="wages" class="form-control" id="wages" value="{{$Employee->wages}}" readonly aria-describedby="wages">
+                    </div>
                 </ul>
             </div>
             <a class="btn btn-success mt3" href="{{route('employee.index')}}">Kembali</a>
