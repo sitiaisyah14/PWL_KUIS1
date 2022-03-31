211dddee3817bf61c76f24d5d0911166be61b4d8
deatrisya <mirelldee47@gmail.com>
2022-03-31 21:40:57 +0700

    Add function for CRUD customer controller

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
@@ -11,9 +12,23 @@ class CustomerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $pagination = 5;
+        $customers = Customer::when($request->keyword, function($query) use ($request){
+            $query
+            ->where('nama_customer','like',"%{$request->keyword}%")
+            ->orWhere('email','like',"%{$request->keyword}%")
+            ->orWhere('gender','like',"%{$request->keyword}%")
+            ->orWhere('tempat_lahir','like',"%{$request->keyword}%")
+            ->orWhere('tgl_lahir','like',"%{$request->keyword}%")
+            ->orWhere('alamat','like',"%{$request->keyword}%")
+            ->orWhere('no_hp','like',"%{$request->keyword}%");
+        })->orderBy('id')->paginate($pagination);
+
+        $customers->appends($request->only('keyword'));
+        return view('customer.customer',compact('customers'))
+            ->with('i',($request->input('page',1)-1)*$pagination);
     }
 
     /**
@@ -23,7 +38,7 @@ class CustomerController extends Controller
      */
     public function create()
     {
-        //
+        return view('customer.customerCreate');
     }
 
     /**
@@ -34,7 +49,33 @@ class CustomerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //melakukan validasi data
+        $request->validate([
+            'foto_customer' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'nama_customer' => 'required',
+            'email' => 'required',
+            'gender' => 'required',
+            'tempat_lahir' => 'required',
+            'tgl_lahir' => 'required',
+            'alamat' => 'required',
+            'no_hp' => 'required'
+        ]);
+
+        //fungsi eloquent untuk menambahkan data
+        $customer = $request->all();
+
+        if ($image = $request->file('foto_customer')) {
+            $destinationPath = 'foto_customer/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $customer['foto_customer'] = "$profileImage";
+        }
+
+        Customer::create($customer);
+
+        //jika data berhasil ditambahkan, akan kembali ke halaman index
+        return redirect()->route('customer.index')
+        -> with('success','Customer Baru Berhasil Ditambahkan');
     }
 
     /**
@@ -45,7 +86,8 @@ class CustomerController extends Controller
      */
     public function show($id)
     {
-        //
+        $customer = Customer::find($id);
+        return view('customer.customerDetail',compact('customer'));
     }
 
     /**
@@ -56,7 +98,8 @@ class CustomerController extends Controller
      */
     public function edit($id)
     {
-        //
+        $customer = Customer::find($id);
+        return view('customer.customerEdit',compact('customer'));
     }
 
     /**
@@ -68,7 +111,34 @@ class CustomerController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        //melakukan validasi data
+        $request->validate([
+            'nama_customer' => 'required',
+            'email' => 'required',
+            'gender' => 'required',
+            'tempat_lahir' => 'required',
+            'tgl_lahir' => 'required',
+            'alamat' => 'required',
+            'no_hp' => 'required'
+        ]);
+
+        //fungsi eloquent untuk menambahkan data
+        $customer = $request->all();
+
+        if ($image = $request->file('foto_customer')) {
+            $destinationPath = 'foto_customer/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $customer['foto_customer'] = "$profileImage";
+        } else {
+            unset($customer['foto_customer']);
+        }
+
+        Customer::find($id)->update($customer);
+
+        //jika data berhasil ditambahkan, akan kembali ke halaman index
+        return redirect()->route('customer.index')
+        -> with('success','Data Customer Berhasil Diupdate');
     }
 
     /**
@@ -79,6 +149,8 @@ class CustomerController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Customer::find($id)->delete();
+        return redirect()->route('customer.index')
+        ->with('success','Customer Berhasil Dihapus');
     }
 }
